aa0d3075b217913b63111dbaa4a5ea1be110ec5d
2Daehan <eogks324@gmail.com>
2024-11-14 22:28:29 +0900

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,17 @@ import 'my.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'login.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized(); // Firebase 초기화 전에 위젯 바인딩 준비
-  await Firebase.initializeApp();(// Firebase 초기화
-  options: DefaultFirebaseOptions.currentPlatform,
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -27,7 +30,8 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(),
+      home : const MyHomePage(),
+      // home: const LoginScreen(), 로그인 페이지로 시작하게 하는
     );
   }
 }
