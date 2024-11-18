22041cf54711a53ec4ba693e2e542edc7e60ec34
VenyVince <redguy0814@gmail.com>
2024-11-18 11:16:46 +0900

    TODO.DART & FIREBASE연동 완료

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+// Todo 모델 클래스
 class Todo {
+  String id;
   String title;
   final DateTime date;
   String? memo;
@@ -9,6 +12,7 @@ class Todo {
   bool isDone;
 
   Todo({
+    this.id = '',
     required this.title,
     required this.date,
     this.memo,
@@ -16,9 +20,43 @@ class Todo {
     this.repeat,
     this.isDone = false,
   });
+
+  // Firestore에서 가져온 데이터를 Map으로 변환
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'date': Timestamp.fromDate(date),  // DateTime을 Timestamp로 변환
+      'memo': memo,
+      'alarmTime': alarmTime != null
+          ? {'hour': alarmTime!.hour, 'minute': alarmTime!.minute}
+          : null,
+      'repeat': repeat,
+      'isDone': isDone,
+    };
+  }
+
+
+  // Firestore의 데이터를 Todo 객체로 변환
+  static Todo fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Todo(
+      id: doc.id,
+      title: data['title'],
+      date: (data['date'] as Timestamp).toDate(),
+      memo: data['memo'],
+      alarmTime: data['alarmTime'] != null
+          ? TimeOfDay(
+        hour: data['alarmTime']['hour'],
+        minute: data['alarmTime']['minute'],
+      )
+          : null,
+      repeat: data['repeat'],
+      isDone: data['isDone'],
+    );
+  }
 }
 
-//TodoPage 수정: 콜백 함수 추가
+// TodoPage 수정: 콜백 함수 추가
 class TodoPage extends StatefulWidget {
   final DateTime selectedDate;
   final Function(Todo) onTodoAdded;
@@ -37,12 +75,52 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
-  //초기 할 일 리스트
-  final List<Todo> _todoList = [];
+  // 할 일 목록 (Firestore에서 가져온 데이터 저장)
+  List<Todo> _todoList = [];
 
-  //할 일 목록 업데이트
+  @override
+  void initState() {
+    super.initState();
+    _loadTodos(); // 앱 시작 시 Firestore에서 데이터 불러오기
+  }
+
   void _updateTodoList() {
     widget.onTodoListChanged(_todoList); // 콜백 함수 호출
+  }
+
+  // Firestore에서 할 일 목록 읽기
+  Future<void> _loadTodos() async {
+    final todoCollection = FirebaseFirestore.instance.collection('todos');
+    QuerySnapshot snapshot = await todoCollection.get();
+
+    setState(() {
+      _todoList = snapshot.docs
+          .map((doc) => Todo.fromFirestore(doc))  // Firestore에서 Todo 객체로 변환
+          .toList();
+    });
+  }
+
+  // Firestore에 할 일 추가
+  Future<void> _addTodoToFirestore(Todo todo) async {
+    final todoCollection = FirebaseFirestore.instance.collection('todos');
+    try {
+      DocumentReference docRef = await todoCollection.add(todo.toMap());
+      setState(() {
+        todo.id = docRef.id;  // Firestore에서 생성된 ID를 Todo에 저장
+      });
+    } catch (e) {
+      print("Error adding todo: $e");
+    }
+  }
+
+  // Firestore에서 할 일 삭제
+  Future<void> _deleteTodoFromFirestore(String todoId) async {
+    final todoCollection = FirebaseFirestore.instance.collection('todos');
+    try {
+      await todoCollection.doc(todoId).delete();
+    } catch (e) {
+      print("Error deleting todo: $e");
+    }
   }
 
   @override
@@ -58,13 +136,14 @@ class _TodoPageState extends State<TodoPage> {
         itemBuilder: (context, index) {
           final todo = filteredTodoList[index];
           return Dismissible(
-            key: UniqueKey(),
+            key: Key(todo.id),
             direction: DismissDirection.endToStart,
             onDismissed: (direction) {
               setState(() {
                 _todoList.remove(todo);
               });
               widget.onTodoRemoved(todo); // 할 일 삭제 콜백 호출
+              _deleteTodoFromFirestore(todo.id); // Firestore에서 할 일 삭제
               _updateTodoList();
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(content: Text('${todo.title} 삭제됨')),
@@ -99,7 +178,7 @@ class _TodoPageState extends State<TodoPage> {
     );
   }
 
-  //할 일 항목의 우측 버튼 구성
+  // 할 일 항목의 우측 버튼 구성
   Widget _buildTrailingButtons(Todo todo) {
     return Row(
       mainAxisSize: MainAxisSize.min,
@@ -121,7 +200,7 @@ class _TodoPageState extends State<TodoPage> {
     );
   }
 
-  //제목 수정 다이얼로그
+  // 제목 수정 다이얼로그
   void _showEditTitleDialog(BuildContext context, Todo todo) {
     TextEditingController _controller = TextEditingController(text: todo.title);
     showDialog(
@@ -154,7 +233,7 @@ class _TodoPageState extends State<TodoPage> {
     );
   }
 
-  //할 일 상세 보기
+  // 할 일 상세 보기
   void _showTodoDetails(BuildContext context, Todo todo) {
     showModalBottomSheet(
       context: context,
@@ -183,7 +262,7 @@ class _TodoPageState extends State<TodoPage> {
     );
   }
 
-  //메모 입력 필드
+  // 메모 입력 필드
   Widget _buildMemoField(Todo todo) {
     return TextFormField(
       initialValue: todo.memo,
@@ -201,7 +280,7 @@ class _TodoPageState extends State<TodoPage> {
     );
   }
 
-  //알림 및 반복 설정 버튼
+  // 알림 및 반복 설정 버튼
   Widget _buildAlarmAndRepeatRow(Todo todo) {
     return Row(
       children: [
@@ -211,44 +290,83 @@ class _TodoPageState extends State<TodoPage> {
       ],
     );
   }
-  //알림 옵션 생성 함수
+
+  // 알림 옵션 생성 함수
   Widget _buildAlarmOption(Todo todo) {
     return Expanded(
       child: GestureDetector(
         onTap: () => _selectAlarmTime(context, todo),
         child: _buildOptionBox(
           icon: Icons.alarm,
-          text: todo.alarmTime != null ? '${todo.alarmTime!.hour}:${todo.alarmTime!.minute}' : '알림 없음',
-        ),
-      ),
-    );
-  }
-  //반복 설정 옵션 생성
-  Widget _buildRepeatOption(Todo todo) {
-    return Expanded(
-      child: GestureDetector(
-        onTap: () => _selectRepeatOption(context, todo),
-        child: _buildOptionBox(
-          icon: Icons.autorenew,
-          text: todo.repeat ?? '반복 없음',
+          text: todo.alarmTime != null ? '${todo.alarmTime!.hour}:${todo.alarmTime!.minute}' : '알림 시간',
         ),
       ),
     );
   }
 
-  //옵션 박스 UI 구성
+  // 반복 옵션 생성 함수
+  Widget _buildRepeatOption(Todo todo) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => _selectRepeatOption(context, todo),
+        child: _buildOptionBox(
+          icon: Icons.repeat,
+          text: todo.repeat ?? '반복 설정',
+        ),
+      ),
+    );
+  }
+
+  // 알림 시간 선택기
+  Future<void> _selectAlarmTime(BuildContext context, Todo todo) async {
+    TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: todo.alarmTime ?? TimeOfDay.now(),
+    );
+    if (newTime != null) {
+      setState(() {
+        todo.alarmTime = newTime;
+        _updateTodoList();
+      });
+    }
+  }
+
+  // 반복 옵션 선택기
+  Future<void> _selectRepeatOption(BuildContext context, Todo todo) async {
+    String? selectedRepeat = await showDialog<String>(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text('반복 설정'),
+          children: [
+            SimpleDialogOption(child: Text('매일'), onPressed: () => Navigator.pop(context, '매일')),
+            SimpleDialogOption(child: Text('매주'), onPressed: () => Navigator.pop(context, '매주')),
+            SimpleDialogOption(child: Text('매월'), onPressed: () => Navigator.pop(context, '매월')),
+            SimpleDialogOption(child: Text('반복 안 함'), onPressed: () => Navigator.pop(context, null)),
+          ],
+        );
+      },
+    );
+    if (selectedRepeat != null) {
+      setState(() {
+        todo.repeat = selectedRepeat;
+        _updateTodoList();
+      });
+    }
+  }
+
+  // 옵션 박스 스타일링
   Widget _buildOptionBox({required IconData icon, required String text}) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.1),
-        borderRadius: BorderRadius.circular(5),
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(8),
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(icon, color: Colors.blue),
-          SizedBox(width: 8),
+          Icon(icon, color: Colors.black),
+          SizedBox(width: 10),
           Text(text),
         ],
       ),
@@ -257,87 +375,38 @@ class _TodoPageState extends State<TodoPage> {
 
   // 완료 버튼
   Widget _buildCompletionButton(Todo todo) {
-    return ElevatedButton.icon(
+    return ElevatedButton(
       onPressed: () {
         setState(() {
           todo.isDone = !todo.isDone;
         });
         _updateTodoList();
-        Navigator.pop(context);
       },
-      icon: Icon(todo.isDone ? Icons.check_circle : Icons.circle_outlined),
-      label: Text(todo.isDone ? '완료 취소' : '완료로 표시'),
+      child: Text(todo.isDone ? '완료 취소' : '완료하기'),
     );
   }
 
-  // 삭제 버튼
+  // 할 일 삭제 버튼
   Widget _buildDeleteButton(Todo todo) {
-    return ElevatedButton.icon(
+    return ElevatedButton(
       onPressed: () {
         setState(() {
           _todoList.remove(todo);
         });
-        widget.onTodoRemoved(todo);
+        widget.onTodoRemoved(todo);  // 삭제된 할 일 콜백 호출
+        _deleteTodoFromFirestore(todo.id);
         _updateTodoList();
         Navigator.pop(context);
       },
-      icon: Icon(Icons.delete, color: Colors.red),
-      label: Text('삭제'),
+      child: Text('삭제'),
       style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.red.withOpacity(0.8),
-      ),
+        backgroundColor: Colors.red, ),
+
     );
   }
 
-  // 알림 시간 선택
-  void _selectAlarmTime(BuildContext context, Todo todo) async {
-    TimeOfDay? selectedTime = await showTimePicker(
-      context: context,
-      initialTime: todo.alarmTime ?? TimeOfDay.now(),
-    );
-    if (selectedTime != null) {
-      setState(() {
-        todo.alarmTime = selectedTime;
-      });
-      _updateTodoList();
-    }
-  }
-
-  // 반복 옵션 선택
-  void _selectRepeatOption(BuildContext context, Todo todo) async {
-    String? selectedRepeat = await showDialog<String>(
-      context: context,
-      builder: (BuildContext context) {
-        return SimpleDialog(
-          title: Text('반복 설정'),
-          children: <Widget>[
-            _buildRepeatOptionDialog('매일'),
-            _buildRepeatOptionDialog('매주'),
-            _buildRepeatOptionDialog('매년'),
-            _buildRepeatOptionDialog('반복 안함'),
-            _buildRepeatOptionDialog('사용자화'),
-          ],
-        );
-      },
-    );
-    if (selectedRepeat != null) {
-      setState(() {
-        todo.repeat = selectedRepeat;
-      });
-      _updateTodoList();
-    }
-  }
-// 반복 설정 옵션 반영
-  SimpleDialogOption _buildRepeatOptionDialog(String option) {
-    return SimpleDialogOption(
-      onPressed: () {
-        Navigator.pop(context, option);
-      },
-      child: Text(option),
-    );
-  }
-
-  // 새로운 할 일 추가
+  // 새 할 일 추가 다이얼로그
+  // 새 할 일 추가 다이얼로그
   void _addNewTodoDialog() {
     TextEditingController _controller = TextEditingController();
 
@@ -357,19 +426,30 @@ class _TodoPageState extends State<TodoPage> {
             ),
             TextButton(
               child: Text('추가'),
-              onPressed: () {
+              onPressed: () async {
                 String title = _controller.text.trim();
 
                 if (title.isNotEmpty) {
-                  setState(() {
-                    Todo newTodo = Todo(
-                      title: title,
-                      date: widget.selectedDate,
+                  // 새로운 할 일 생성
+                  Todo newTodo = Todo(
+                    title: title,
+                    date: widget.selectedDate,
+                  );
+
+                  try {
+                    // Firestore에 새로운 할 일 추가 후 setState 호출
+                    await _addTodoToFirestore(newTodo);  // Firestore에 추가
+                    setState(() {
+                      _todoList.add(newTodo); // 할 일 목록에 추가
+                    });
+                    widget.onTodoAdded(newTodo); // 새 할 일 추가 콜백 호출
+                    Navigator.of(context).pop();
+                  } catch (e) {
+                    // 오류 처리
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('오류가 발생했습니다: $e'))
                     );
-                    _todoList.add(newTodo);
-                    widget.onTodoAdded(newTodo); // 추가된 할 일 콜백 호출
-                  });
-                  Navigator.of(context).pop();
+                  }
                 } else {
                   ScaffoldMessenger.of(context).showSnackBar(
                       SnackBar(content: Text('할 일 제목을 입력해주세요!'))
@@ -382,9 +462,8 @@ class _TodoPageState extends State<TodoPage> {
       },
     );
   }
-}
 
-// 두 날짜가 같은 날인지 확인하는 함수
-bool isSameDay(DateTime a, DateTime b) {
-  return a.year == b.year && a.month == b.month && a.day == b.day;
+  bool isSameDay(DateTime date1, DateTime date2) {
+    return date1.year == date2.year && date1.month == date2.month && date1.day == date2.day;
+  }
 }
